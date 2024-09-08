6fe7f2965afe713eef7d33b0959c447505d7dba4
Alexander Lohnau <alexander.lohnau@gmx.de>
2024-09-08 12:10:33 +0200

    Add py.typed marker

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='krunnerdbusutils',
-    version='0.1.0',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'dbus-python',
